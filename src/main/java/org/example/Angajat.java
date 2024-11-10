1a98c11e7be56e8877b45aa15a7abb1094ac8e25
Bosky19 <82734739+Bosky19@users.noreply.github.com>
2024-11-10 15:38:08 +0200

    cx

--- src/main/java/org/example/Angajat.java
+++ src/main/java/org/example/Angajat.java
@@ -1,4 +1,44 @@
 package org.example;
 
 public class Angajat {
+    private String nume;
+    private String post;
+    private String data_angajarii;
+    private float salar;
+    public Angajat(){}
+    public Angajat(String nume, String post, String data_angajare, float Salariu) {
+        super();
+        this.nume = nume;
+        this.post = post;
+        this.data_angajarii = data_angajare;
+        this.salar = Salariu;
+    }
+    public String getNume() {
+        return nume;
+    }
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+    public String getPost() {
+        return post;
+    }
+    public void setPost(String post) {
+        this.post = post;
+    }
+    public String getData_angajarii() {
+        return data_angajarii;
+    }
+    public void setData_angajarii(String data_angajarii) {
+        this.data_angajarii = data_angajarii;
+    }
+    public float getSalar() {
+        return salar;
+    }
+    public void setSalar(float salar) {
+        this.salar = salar;
+    }
+    @Override
+    public String toString() {
+        return this.nume + "," + this.post + "," + this.data_angajarii + "," + this.salar;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,130 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void scriere(List<Angajat> lista){
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            File file = new File("src/main/resources/angajati.json");
+            mapper.writeValue(file,lista);
+        } catch (IOException e){
+            e.printStackTrace();
         }
+    }
+    public static List<Angajat> citire(){
+        try {
+            File file = new File("src/main/resources/angajati.json");
+            ObjectMapper mapper = new ObjectMapper();
+            List<Angajat> lista = mapper.readValue(file, new TypeReference<List<Angajat>>(){});
+            return lista;
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        List<Angajat> listaAngajat = citire();
+        List<Angajat> listapct3 = new ArrayList<Angajat>();
+        Scanner scanner = new Scanner(System.in);
+        int x;
+        while(true)
+        {
+            System.out.println("1.Afisare lista");
+            System.out.println("2.Afisare angajati peste 2500");
+            System.out.println("3.creare lista din aprilie anu trecut");
+            System.out.println("4.care nu sunt sefi salar descresc");
+            System.out.println("5.nume angajati majuscule afisare");
+            System.out.println("6. salarii mai mici de 3000");
+            x=scanner.nextInt();
+            switch(x)
+            {
+                case 1: {
+                    for(Angajat angajat : listaAngajat)
+                    {
+                        System.out.println(angajat);
+                    }
+                    break;
+                }
+                case 2: {
+                    for(Angajat angajat : listaAngajat) {
+                        if(angajat.getSalar()>2500)
+                        {
+                            System.out.println(angajat);
+                        }
+                    }
+                    break;
+                }
+                case 3:{
+                    String an;
+                    int ani= LocalDate.now().getYear()-1;
+                    an = String.valueOf(ani);
+                    listapct3 = new ArrayList<Angajat>();
+                    for(Angajat angajat : listaAngajat) {
+                        String split[]=angajat.getData_angajarii().split("-");
+                        if(split[0].equals(an))
+                        {
+                            if(split[1].equals("04"))
+                            {
+                                if(angajat.getPost().contains("sef")||angajat.getPost().contains("director")) {
+                                    listapct3.add(angajat);
+                                }
+                            }
+                        }
+
+                    }
+                    for(Angajat angajat : listapct3) {
+                        System.out.println(angajat);
+                    }
+                    break;
+                }
+                case 4:
+                {
+                    List<Angajat> angajatiFaraFunctieConducere = listaAngajat.stream()
+                            .filter(angajat -> !angajat.getPost().toLowerCase().contains("sef") &&
+                                    !angajat.getPost().toLowerCase().contains("director"))
+                            .sorted(Comparator.comparingDouble(Angajat::getSalar).reversed())
+                            .collect(Collectors.toList());
+                    for(Angajat angajat: angajatiFaraFunctieConducere) {
+                        System.out.println(angajat);
+                    }
+                    break;
+                }
+                case 5:
+                {
+                    List<String> numeAngajatiMajuscule = listaAngajat.stream()
+                            .map(angajat -> angajat.getNume().toUpperCase())
+                            .collect(Collectors.toList());
+
+                    numeAngajatiMajuscule.forEach(System.out::println);
+                    break;
+                }
+                case 6:
+                {
+                    List<Angajat> angajaticv = listaAngajat.stream().filter(angajat -> angajat.getSalar() < 3000).collect(Collectors.toList());
+                    angajaticv.forEach(angajat -> System.out.println(angajat.getSalar()));
+
+                    break;
+                }
+                case 7:
+                {
+                    break;
+                }
+                default: {break;}
+
+            }
+        }
+
     }
 }
